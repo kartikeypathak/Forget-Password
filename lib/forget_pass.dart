5697c0f34bdbb468045e8576f1fdc42c0036ef1f
kartikey pathak <kartikeypathak007@gmail.com>
2022-07-04 15:09:21 +0530

    Added New Files to project

---
+++ lib/forget_pass.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ForgetPassword extends StatefulWidget {
+  const ForgetPassword({Key? key}) : super(key: key);
+
+  @override
+  State<ForgetPassword> createState() => _ForgetPasswordState();
+}
+
+class _ForgetPasswordState extends State<ForgetPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text('Enter your e-mail'),
+    );
+  }
+}
